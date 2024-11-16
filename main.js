51855551f5bf084162fcc7b738cee8a9844bca61
Yousof Akbarzai <yousofakbarzai@gmail.com>
2024-11-15 19:29:20 -0500

    text geometry of 'Yousof's Webpage'

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
   const fov = 75;
   const aspect = 2;
   const near = 0.1;
-  const far = 5;
+  const far = 15;
   const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
   camera.position.z = 2;
   camera.position.x = 3;
@@ -22,6 +24,25 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
     const line = new THREE.Line(geometry,material);
     scene.add(line);
   }
+  const loader = new FontLoader();
+  loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+	const geometry = new TextGeometry( "Yousof's Webpage", {
+		font: font,
+		size: 2,
+		depth: 5,
+		curveSegments: 12,
+		bevelEnabled: false,
+	} );
+	geometry.center();
+	const normMaterial = new THREE.MeshNormalMaterial();
+        const wordMesh = new THREE.Mesh(geometry,normMaterial);
+	wordMesh.position.z = -120;
+        wordMesh.position.x = 3;
+	wordMesh.position.y = 3;
+        scene.add(wordMesh);
+} );
+  
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
@@ -34,9 +55,8 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
   }
   function render(time) {
     time *= 0.001;
-    camera.position.z -= time;
-    if (camera.position.z < -100) {
-	document.getElementById('links').style.display = "block";
+    if (camera.position.z > -105) {
+      camera.position.z -= time;
     }
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -47,3 +67,4 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
+
